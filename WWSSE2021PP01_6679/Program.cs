6fed89bf129e5c6e6ba137348d71ecdaa5108472
ajax-git <ajax.grandtheftauto@gmail.com>
2021-12-19 10:24:52 +0100

    Dodaj pliki projektów.

---
+++ WWSSE2021PP01_6679/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+    Napisz program wyświetlający na konsoli następujący kształt:
+    X
+    XX
+    XXX
+    XXXX
+    XXXXX
+*/
+
+string text = "X";
+Console.WriteLine(text);
+
+for(int i = 1; i <= 4; i++)
+{
+    text = text + 'X';
+    Console.WriteLine(text);
+}
